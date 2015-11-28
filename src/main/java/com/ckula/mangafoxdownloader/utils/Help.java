2438dc1d597e93790d387e15426dd07ec9d7c9dd
Christian Kula <christian.kula@edu.itescia.fr>
2015-11-28 12:48:21 +0100

    Script added at the root of the git - Change in displaying the help
    The script can be downloaded directly from the project repository
    The displaying of the help has been changed

--- src/main/java/com/ckula/mangafoxdownloader/utils/Help.java
+++ src/main/java/com/ckula/mangafoxdownloader/utils/Help.java
@@ -3,55 +3,86 @@ package com.ckula.mangafoxdownloader.utils;
 public class Help {
 
     public static void printHelp() {
-	System.out.println(
-		"This script downloads mangas from mangafox.me. You must have the JRE 1.6 (or higher) installed.");
-	System.out.println();
-	System.out.println(
-		"SYNTAX\r\njava -jar mangafox_downloader.jar [REQUIRED] <manga name> [OPTIONAL] -v <volume number> -c <chapitre number>");
-	System.out.println();
-	System.out.println();
 
-	System.out.println("|OPTIONS|");
-	System.out.println();
+	StringBuilder help = new StringBuilder();
 
-	System.out.println(
+	help.append("This script downloads mangas from mangafox.me. You must have the JRE 1.6 (or higher) installed.");
+	help.append(System.lineSeparator());
+
+	help.append(System.lineSeparator());
+	help.append("SYNTAX");
+	help.append(System.lineSeparator());
+	help.append(
+		"java -jar mangafox_downloader.jar [REQUIRED] <manga name> [OPTIONAL] -v <volume number> -c <chapitre number>");
+	help.append(System.lineSeparator());
+
+	help.append(System.lineSeparator());
+	help.append(System.lineSeparator());
+
+	help.append("OPTIONS");
+	help.append(System.lineSeparator());
+	help.append(System.lineSeparator());
+
+	help.append(
 		"[REQUIRED] <manga name> \r\nRefers to the name of the manga you want to dowload as displayed on mangafox.me");
-	System.out.println("Case insensitive and can contain space but not special characters");
-	System.out.println(
+	help.append(System.lineSeparator());
+
+	help.append("Case insensitive and can contain space but not special characters");
+	help.append(System.lineSeparator());
+
+	help.append(
 		"Tip : To be sure you got it right, visit the desired manga page on mangafox.me. The name of the manga appears in the URL in this form : www.mangafox.me/manga/<manga name>/");
+	help.append(System.lineSeparator());
 
-	System.out.println();
-	System.out.println("EXAMPLES");
-	System.out.println("java -jar mangafox_downloader.jar naruto");
-	System.out.println(
-		"java -jar mangafox_downloader.jar \"Kangoku Gakuen\" - Kangoku Gakuen refers to Prison School");
+	help.append(System.lineSeparator());
 
-	System.out.println();
-	System.out.println();
+	help.append("EXAMPLES");
+	help.append(System.lineSeparator());
+	help.append("java -jar mangafox_downloader.jar naruto");
+	help.append(System.lineSeparator());
+	help.append("java -jar mangafox_downloader.jar \"Kangoku Gakuen\" - Kangoku Gakuen refers to Prison School");
+	help.append(System.lineSeparator());
 
-	System.out.println(
+	help.append(System.lineSeparator());
+	help.append(System.lineSeparator());
+
+	help.append(
 		"[OPTIONAL] -v <volume number> \r\nUse this option to download only the specified volume of a manga (= every chapter of the specified volume)");
-	System.out.println("The volume number is as displayed on the manga page. Ranging from 0 to n");
-	System.out.println("The volume number can also be \"TBD\" (refering to \"Volume TBD\" - To be determined)");
-	System.out.println("The volume number can also be \"NA\" (refering to \"Volume Not Available\")");
-	System.out.println();
-	System.out.println("EXAMPLES");
-	System.out.println("java -jar mangafox_downloader.jar naruto -v NA");
-	System.out.println("java -jar mangafox_downloader.jar \"Kangoku Gakuen\" -v 18");
 
-	System.out.println();
-	System.out.println();
+	help.append(System.lineSeparator());
+	help.append("The volume number is as displayed on the manga page. Ranging from 0 to n");
+	help.append("The volume number can also be \"TBD\" (refering to \"Volume TBD\" - To be determined)");
+	help.append(System.lineSeparator());
+	help.append("The volume number can also be \"NA\" (refering to \"Volume Not Available\")");
+	help.append(System.lineSeparator());
+	help.append(System.lineSeparator());
+	help.append("EXAMPLES");
+	help.append(System.lineSeparator());
+	help.append("java -jar mangafox_downloader.jar naruto -v NA");
+	help.append(System.lineSeparator());
+	help.append("java -jar mangafox_downloader.jar \"Kangoku Gakuen\" -v 18");
 
-	System.out.println(
+	help.append(System.lineSeparator());
+	help.append(System.lineSeparator());
+
+	help.append(
 		"[OPTIONAL] -v <volume number> -c <chapter number>\r\nUse this option to download only the specified chapter of the specified volume of a manga");
-	System.out.println(
+	help.append(System.lineSeparator());
+	help.append(
 		"When using -c, -v is mandatory as some mangas' chapters numbers aren't unique (i.e. the Dective Conan manga has multiple \"Chapter 1\" who knows why)");
-	System.out.println("Be sure the specified chapter is contained within the specified volume");
-	System.out.println("The chapter number is as displayed on the manga page. Ranging from 0 to n");
-	System.out.println();
-	System.out.println("EXAMPLES");
-	System.out.println("java -jar mangafox_downloader.jar naruto -v 60 -c 575");
-	System.out.println("java -jar mangafox_downloader.jar \"Kangoku Gakuen\" -v TBD -c 197");
+	help.append(System.lineSeparator());
+	help.append("Be sure the specified chapter is contained within the specified volume");
+	help.append(System.lineSeparator());
+	help.append("The chapter number is as displayed on the manga page. Ranging from 0 to n");
+	help.append(System.lineSeparator());
+	help.append(System.lineSeparator());
+	help.append("EXAMPLES");
+	help.append(System.lineSeparator());
+	help.append(System.lineSeparator());
+	help.append("java -jar mangafox_downloader.jar naruto -v 60 -c 575");
+	help.append(System.lineSeparator());
+	help.append("java -jar mangafox_downloader.jar \"Kangoku Gakuen\" -v TBD -c 197");
 
+	System.out.println(help);
     }
 }
